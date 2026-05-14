def0afb6e4fd2df0ed5a00e64947751e91b70726
Henrique Luiz Gallassini <109484780+henriquegallassini@users.noreply.github.com>
2026-05-14 09:44:14 -0300

    Update length condition for function size warning

--- AI Reviewer-CLI.py
+++ AI Reviewer-CLI.py
@@ -29,7 +29,7 @@ def analisar_codigo(caminho_arquivo):
                 )
 
             tamanho = no.end_lineno - no.lineno
-            if tamanho > 5 and <= 20:
+            if tamanho > 20:
                     tabela.add_row(
                         str(no.lineno),
                         f"📏 Função muito longa ({tamanho} linhas)",
